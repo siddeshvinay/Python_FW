988ff1bd6d7b1e73bd9f9f215135e2d30eda2bc6
Siddeshvinay <siddeshvinay@gmail.com>
2018-08-21 18:04:23 +0530

    new code

--- TMS_SP_MainScripts/TestCase1.py
+++ TMS_SP_MainScripts/TestCase1.py
@@ -4,21 +4,23 @@ from TMS_SP import Locators
 from time import sleep
 from utility import ApplicationIndependent
 from TMS_ProjectSpecificScripts import Login_scripts
-
+import os
 
 def Testcase1(self):
     Login_scripts.LaunchBrowser(self)
     sleep(3)
     Login_scripts.navigate(self)
     sleep(3)
-    Login_scripts.Username(self).send_keys("sp@gtnexus")
+    Login_scripts.Username(self).send_keys(os.environ.get('Username', -1))
+    #Login_scripts.Username(self).send_keys("sp@gtnexus")
     sleep(2)
-    Login_scripts.password(self).send_keys("bfqFDIZWkv6st@")
+    Login_scripts.password(self).send_keys(os.environ.get('Password', -1))
+    #Login_scripts.password(self).send_keys("bfqFDIZWkv6st@")
     sleep(2)
     Login_scripts.login_button(self)
 
 
-Testcase1(webdriver)
+
 
 
 
